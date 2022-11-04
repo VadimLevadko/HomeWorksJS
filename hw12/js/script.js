857941c5f165888bee088b9f8d4fece17b22acab
VadimLevadko <vadimlevadko@gmail.com>
2022-11-05 00:40:58 +0200

    JS DOM Home Work

--- hw12/js/script.js
+++ hw12/js/script.js
@@ -2,6 +2,24 @@ const generateArr = [1,2,[1.1, 1.2, 1.3],3]
 
 generateList(generateArr)
 
+function getNewArr(someArr) {
+    const li = document.createElement('li')
+    const newUl = document.createElement('ul')
+
+    list.append(li)
+    li.append(newUl)
+    
+    someArr.forEach(newElem => {
+        if(Array.isArray(newElem)) {
+            getNewArr(newElem)
+        } else {
+            let li = document.createElement('li')
+            li.innerHTML = newElem
+            newUl.append(li)
+        }
+    })
+}
+
 function generateList(array) {
     const list = document.getElementById('list')
 
@@ -9,23 +27,6 @@ function generateList(array) {
 
         if(Array.isArray(element)) {
             getNewArr(element)
-            function getNewArr(someArr) {
-                const li = document.createElement('li')
-                const newUl = document.createElement('ul')
-
-                list.append(li)
-                li.append(newUl)
-                
-                someArr.forEach(newElem => {
-                    if(Array.isArray(newElem)) {
-                        getNewArr(newElem)
-                    } else {
-                        let li = document.createElement('li')
-                        li.innerHTML = newElem
-                        newUl.append(li)
-                    }
-                })
-            }
         } else {
             let test = document.createElement('li')
             test.innerHTML = element
@@ -34,20 +35,23 @@ function generateList(array) {
     });
 }
 
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
 createTable()
 
 function createTable() {
     const table = document.getElementById('table')
-    let newUl = ''
+    let newTr = ''
 
     for(let i = 0; i < 100; i++) {
         if(i % 10 === 0) {
-            newUl = document.createElement('ul')
-            table.append(newUl)
+            newTr = document.createElement('tr')
+            table.append(newTr)
         } 
 
-        let li = document.createElement('li')
-        li.innerHTML = i + 1
-        newUl.append(li)
+        let td = document.createElement('td')
+        td.innerHTML = i + 1
+        td.classList.add('td-border')
+        newTr.append(td)
     }
 }
